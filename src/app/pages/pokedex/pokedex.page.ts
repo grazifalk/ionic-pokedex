a0586e362051c1fe7ba8696a8f98d0d0f10b475e
Grazi Falk <83370784+grazifalk@users.noreply.github.com>
2024-01-19 12:09:05 -0300

    :tada: base with routes, service and main page

---
+++ src/app/pages/pokedex/pokedex.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from 'src/app/services/pokeapi/pokeapi.service';
+
+@Component({
+  selector: 'app-pokedex',
+  templateUrl: './pokedex.page.html',
+  styleUrls: ['./pokedex.page.scss'],
+})
+export class PokedexPage implements OnInit {
+
+  pokemons: any = [];
+
+  constructor(private pokeapiService: PokeapiService) { 
+    
+  }
+
+  getPokedex() {
+    this.pokeapiService.getPokedex().then((data: any) => {
+      this.pokemons = data.pokemon_entries;
+    }).catch((err) => {
+      console.error(err);
+    })
+  }
+
+  ngOnInit() {
+    this.getPokedex();
+
+  }
+
+}
